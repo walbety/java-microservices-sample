63535b4f89f66643161297af776ed3e4c5a02bca
walbetyaraujo-mbciet <walbety.araujo@ext.mercadobitcoin.com.br>
2022-01-19 16:31:53 -0300

    initial commit - added base structure created with spring initializr

---
+++ src/main/java/com/walbety/micros/MicrosApplication.java
@@ -0,0 +1,13 @@
+package com.walbety.micros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/walbety/micros/MicrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walbety.micros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
